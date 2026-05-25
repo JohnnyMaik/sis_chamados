d0c0fc98e35d8126db1fc0b2af246026714a66a4
Johnny Maik <maikjohnny'131@gmail.com>
2026-05-25 19:31:21 -0300

    aula 2505

--- app/index.js
+++ app/index.js
@@ -3,22 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const pool = require('./database/db');
 
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
-// const ticketRoutes = require('./routes/ticketRoutes');
-// const cors = require('cors');
-
 const app = express();
 app.use(express.json());
-// app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Sistema chamados' });
-});
-
-// Healthcheck
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', message: 'API rodando' });
 });
 
 // Teste do banco
@@ -30,11 +19,6 @@ app.get('/db', async (req, res) => {
     res.status(500).json({ db: 'erro', detail: err.message });
   }
 });
-
-// Rotas
-app.use(authRoutes);
-app.use(userRoutes);
-// app.use(ticketRoutes);
 
 const PORT = Number(process.env.PORT || 3000);
 app.listen(PORT, () => {
